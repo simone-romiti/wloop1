6798fdb6985e44bb88bd6ccc6f4319b733a5a434
Simone <simone.romiti.1994@gmail.com>
2022-05-22 22:48:18 +0200

    Ho generato i plot di M_eff per il correlatore del pione. Controllare perché sembra non esserci un plateau.

---
+++ C_pi.py
@@ -0,0 +1,90 @@
+
+from ast import For
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+import matplotlib.pyplot as plt
+
+import os
+import sys
+
+HOME_DIR = os.path.expanduser("~/")
+sys.path.insert(0, HOME_DIR+'Documents/zwiebel/')
+import jkf
+from eff_curve import Meff as Meff
+
+from io import StringIO
+
+
+g_Druns = "./data/runs/" #directory of runs
+g_nstep = 100 # measure after each 100 configurations
+g_N_conf = 200 # number of gauge configurations considered
+g_N_therm = 300 # number of configurations before thermalization
+g_N_jkf = 20 # number of jackknifes
+g_L, g_T = 8, 16 # spatial and temporal sizes of the lattice
+
+def dir_path_pattern(beta, L, T):
+    return g_Druns+"X{X}Y{Y}Z{Z}T{T}/".format(X=L,Y=L,Z=1,T=T)+"beta{beta}".format(beta=beta)+"_PureGauge_heat0_nmeas20500_nsteps1000/"
+
+class pion_corr:
+  """ class for the analysis of the pion correlator """
+  def __init__(self, beta, N_jkf, L, T):
+    self.beta = beta
+    self.L, self.T = L, T
+    self.N_jkf = N_jkf
+    self.DIR = dir_path_pattern(beta, L, T)
+    #
+    C_conf = np.zeros((T, g_N_conf))
+    for ig in range(g_N_conf):
+        idx = g_N_therm + ig*g_nstep
+        C_conf[:,ig] = pd.read_csv(self.DIR+"C_pion.{:06d}".format(idx), sep=" ")["C(t)"]
+    ##
+    self.C_jkf = jkf.confs_to_jkf(C_conf, N_jkf)
+    self.plateau = [1, 5]
+  ##
+  def plot_Corr(self, yscale, path):
+    fig, ax = plt.subplots()
+    ax.set_title("${L}^2$x{T} - $\\beta=${beta}".format(L=self.L, T=self.T, beta=self.beta))
+    ax.set_xlabel("t/a")
+    ax.set_ylabel("C(t)")    
+    ax.set_yscale(yscale)
+    ax.errorbar(x=[t for t in range(self.T)], y=self.C_jkf.avrs(), yerr=self.C_jkf.errs(), linestyle="--", capsize=0.5)
+    plt.savefig(path)
+    plt.close()
+  ##
+  def get_M_eff(self):
+    return (self.C_jkf).M_eff_no_bkw()
+  ##
+  def get_fit_M_eff(self, r):
+    E = self.get_M_eff()
+    t1, t2 = self.plateau
+    E.fit_to_const(t1=t1, t2=t2)
+    return E.get_M0()
+  ##
+  def plot_M_eff(self, path):
+    fig, ax = plt.subplots()
+    ax.set_title("${L}^2$x{T} - $\\beta=${beta}".format(L=self.L, T=self.T, beta=self.beta))
+    ax.set_xlabel("t/a")
+    ax.set_ylabel("M_eff(t)")    
+    M_eff = self.get_M_eff()
+    ax.errorbar(x=[t for t in range(self.T-1)], y=M_eff.avrs(), yerr=M_eff.errs(), linestyle="--", capsize=0.5)
+    plt.savefig(path)
+    plt.close()
+##
+
+beta_list = ["1.0", "1.5", "2.0", "2.5", "3.0"]
+for beta in beta_list:
+  print("beta =", beta)
+  PC = pion_corr(beta, g_N_jkf, g_L, g_T)
+  # plots
+  dir = "./plots/beta{beta}/".format(beta=beta)
+  if not os.path.exists(dir):
+    os.makedirs(dir)
+  ##
+  PC.plot_Corr("linear", dir+"Corr.pdf")
+  PC.plot_Corr("log", dir+"Corr_log.pdf")
+  PC.plot_M_eff(dir+"M_eff.pdf")
+#   # effective mass fitting
+#   PC.fit_Meff()
+##
---
+++ correlator.py
@@ -0,0 +1,152 @@
+# correlator.py
+# class for the analysis of a euclidean correlator C_T(t)
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+import matplotlib.pyplot as plt
+
+import os
+import sys
+
+HOME_DIR = os.path.expanduser("~/")
+sys.path.insert(0, HOME_DIR+'Documents/zwiebel/')
+import jkf
+from eff_curve import Meff as Meff
+
+from io import StringIO
+
+class correlator:
+  """ class for the analysis of a euclidean correlator C_T(t) """
+  def __init__(self, N_jkf, T):
+    self.LIST_df_Wr = self.get_list_df_conf_Wr() # list of gauge config data frame for W_r(t)
+    self.plateau = pd.read_csv(DIR_analysis+DIR_geom+"plateau.txt", sep=" ", dtype={'beta': str})
+  ##
+  def get_df_conf_Wr(self, i):
+    F = self.DIR+base_FILE+"{:04d}".format(i)+".dat"
+    sF = open(F, "r").read()
+    sF = sF.replace(" \n", "\n").replace(4*" ", " ").replace(3*" "," ")
+    #
+    TESTDATA = StringIO(sF)
+    return pd.read_csv(TESTDATA, sep=" ")
+  ##  
+  def get_list_df_conf_Wr(self):
+    L_Wr = []
+    for i in range(1, Nconf+1):
+      Wr = self.get_df_conf_Wr(i*Nstep).drop(['t'], axis=1) # no need for time
+      L_Wr.append(Wr)
+    ##
+    return L_Wr
+  ##
+  def get_conf_Wr(self, r):
+    cWr = np.zeros((Lt-1, Nconf))
+    for g in range(Nconf):
+        cWr[:, g] = self.LIST_df_Wr[g]["r={r}".format(r=r)].to_numpy()
+    ##
+    return cWr
+  ##
+  def get_jkf_Wr(self, r):
+    return jkf.confs_to_jkf(self.get_conf_Wr(r), N_jkf)
+  ##
+  def get_jkf_Vr(self, r):
+    Wr = self.get_jkf_Wr(r)
+    return Wr.M_eff_no_bkw()
+  ##
+  def add_time_series(self, Fig, y, ey, lbl, type="linear", tstart=1):
+    """ add plot of planar W_r(t) to the Fig """
+    #
+    x = [i for i in range(tstart, len(y)+1)]
+    #
+    trace_name = lbl
+    Fig.add_trace(
+      go.Scatter(
+        x=x, y=y, 
+        error_y = dict(
+          type = 'data',
+          array = ey,
+          visible=True
+        ),
+        mode="lines+markers+text",
+        name=trace_name
+      )
+    )
+    #
+    return None
+  ##
+  def gen_plot_obs_r(self, fun_Mr, name, type="linear"):
+    """ Generate the plots from the jkf matrix obtained with fun_Mr(r), for all r
+        type = One of the following enumeration values:
+            ['-', 'linear', 'log', 'date', 'category',
+            'multicategory']
+    """
+    Fig = go.Figure()
+    for r in range(1, Lx):
+      y  = fun_Mr(r).ae()["av"]
+      ey = fun_Mr(r).ae()["err"] 
+      self.add_time_series(Fig, y, ey, "{name}_{r}(t)".format(name=name, r=r))
+    ##
+    Fig.update_xaxes(title_text="t")
+    Fig.update_yaxes(title_text=name, type=type)
+    Fig.update_layout()
+    ##
+    dir = "./plots/"+DIR_geom+"beta"+self.beta+"/"
+    if not os.path.exists(dir):
+      os.makedirs(dir)
+    ##
+    outfile = dir+"{name}".format(name=name)
+    if(type !=  "linear"):
+      outfile += "({tp}_plot)".format(tp=type)
+    ##
+    outfile += ".html"
+    Fig.write_html(outfile)
+    return
+  ##
+  def gen_plot_Wr(self, type):
+    self.gen_plot_obs_r(self.get_jkf_Wr, "W", type=type)
+  ##
+  def gen_plot_Vr(self):
+    self.gen_plot_obs_r(self.get_jkf_Vr, "V_eff")
+  ##
+  def get_fit_V(self, r):
+    E = Meff(self.get_jkf_Vr(r))
+    ib = list(self.plateau["beta"]).index(self.beta)
+    t1, t2 = self.plateau.iloc[ib][["t1", "t2"]]
+    E.fit_to_const(t1=t1, t2=t2)
+    return E.get_M0()
+  ##
+  def fit_all_V(self, rlist) -> None:
+    nr = len(rlist)
+    V = jkf.matrix(nr, N_jkf)
+    print("Fitting V(r):")
+    for i in range(nr):
+      print("r=", i)
+      V[i] = self.get_fit_V(rlist[i])
+    ##
+    columns = ["j={j}".format(j=j) for j in range(N_jkf)]
+    V.to_DataFrame(columns=columns).to_csv(DIR_analysis+DIR_geom+"/Vr.dat")
+
+    V.ae().to_csv(DIR_analysis+DIR_geom+"/Vr(ae).dat")
+    plt.title("{X}x{Y}x{Z}x{T} - $\\beta=${beta}".format(X=Lx,Y=Ly,Z=Lz,T=Lt,beta=self.beta))
+    plt.xlabel("r")
+    plt.ylabel("V(r)")    
+    plt.errorbar(x=[r for r in range(1,nr+1)], y=V.ae()["av"], yerr=V.ae()["err"])
+    plt.savefig("./plots/"+DIR_geom+"beta"+beta+"/Vr.pdf")
+    plt.close()
+
+    return None
+##
+
+beta_list = ["1.0", "1.5", "2.0", "2.5", "3.0"]
+for beta in beta_list:
+  print("beta =", beta)
+  Si = static_potential(beta)
+  # plots
+  Si.gen_plot_Wr("linear")
+  Si.gen_plot_Wr("log")
+  Si.gen_plot_Vr()
+  # effective mass fitting
+  Si.fit_all_V([1,2,3])
+##
+
+
